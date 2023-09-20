e533f0348f38a9e119859d9faa15234fb6e9cabb
rachan2005 <22k30.rachan@sjec.ac.in>
2023-09-21 01:24:19 +0530

    1.45

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-import speech_recognition as sr
+from stt import convert_speech_to_text
 from model import chatmodel
 import pyaudio
 from gtts import gTTS
 import time 
 
-
-def convert_speech_to_text():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Speak something:")
-        audio = recognizer.listen(source)
-    try:
-        user_input = recognizer.recognize_google(audio)
-        return user_input
-    except sr.UnknownValueError:
-        error_feed="Could not understand audio"
-        print(error_feed)
-        tts = gTTS(text=error_feed, lang='en')
-        tts.save('error.mp3')
-        return None
-    except sr.RequestError as e:
-        error_feed="Could not request results; {0}".format(e)
-        print(error_feed)
-        tts = gTTS(text=error_feed, lang='en')
-        tts.save('error.mp3')
-        return None
 
 def main():
     while True:
---
+++ stt.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+from gtts import gTTS
+
+def convert_speech_to_text():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak something:")
+        audio = recognizer.listen(source)
+    try:
+        user_input = recognizer.recognize_google(audio)
+        print(user_input)
+        return user_input
+    except sr.UnknownValueError:
+        error_feed="Could not understand audio"
+        print(error_feed)
+        tts = gTTS(text=error_feed, lang='en')
+        tts.save('error.mp3')
+        return None
+    except sr.RequestError as e:
+        error_feed="Could not request results; {0}".format(e)
+        print(error_feed)
+        tts = gTTS(text=error_feed, lang='en')
+        tts.save('error.mp3')
+        return None
